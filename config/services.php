c885d7b77efd2478b66eae6e8053e2d155a76df9
Azri Ismail <azri@fashionvalet.com>
2016-03-09 17:16:15 +0800

    remove default magento url from config

--- config/services.php
+++ config/services.php
@@ -4,6 +4,6 @@ return [
     'magento' => [
         'username' => 'warehouse',
         'password' => '0gb60fz4jp0ecwp0hzy0lzjygguute73',
-        'wsdl'     => env('MAGENTO_SITE_URL', 'http://www.fashionvalet.com') . '/api/v2_soap?wsdl',
+        'wsdl'     => env('MAGENTO_SITE_URL') . '/api/v2_soap?wsdl',
     ],
 ];
